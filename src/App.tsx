a875c5f4b0ab0160838f7de221d3783907c0e162
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-09-20 07:36:15 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,105 +1,27 @@
-import { useState, useEffect, useRef } from "react";
-import {
-  type ModelStatus,
-  type PayloadFromWorker,
-  type PayloadFromMain,
-} from "./types";
-
-const CONTEXT = "You are a helpful assistant.";
+import { useEffect } from "react";
+import { pipeline } from "@huggingface/transformers";
 
 function App() {
-  const [modelStatus, setModelStatus] = useState<ModelStatus>({
-    status: "initiate",
-    progress: 0,
-    modelName: "",
-  });
-  const [outputText, setOutputText] = useState("");
-  const [inputText, setInputText] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const worker = useRef<Worker>(null);
-
   useEffect(() => {
-    // Create the worker if it does not yet exist.
-    worker.current ??= new Worker(new URL("./worker.js", import.meta.url), {
-      type: "module",
-    });
-
-    // Create a callback function for messages from the worker thread.
-    const onMessageReceived = (e: MessageEvent<PayloadFromWorker>) => {
-      switch (e.data.status) {
-        case "load_model":
-          if (typeof e.data.payload === "string") break;
-          setModelStatus(e.data.payload);
-          break;
-        case "start_generate_text":
-          setIsLoading(true);
-          break;
-        case "update_text":
-          if (typeof e.data.payload !== "string") break;
-          setOutputText((prev) => prev + e.data.payload);
-          break;
-        case "done_generate_text":
-          setIsLoading(false);
-          break;
-      }
-    };
-
-    // Attach the callback function as an event listener.
-    worker.current.addEventListener("message", onMessageReceived);
-
-    // Define a cleanup function for when the component is unmounted.
-    return () =>
-      worker.current?.removeEventListener("message", onMessageReceived);
-  });
-
-  function handleType(e: any) {
-    setInputText(e.target.value.trim());
-  }
-
-  async function handleSubmit() {
-    if (!inputText || !worker.current) return;
-    setOutputText("");
-    worker.current.postMessage({
-      inputText,
-      context: CONTEXT,
-    } as PayloadFromMain);
-  }
-
-  function handleKeyDown(e: any) {
-    if (e.key === "Enter") {
-      handleSubmit();
+    async function run() {
+      const generator = await pipeline("text-generation", "Xenova/distilgpt2", {
+        progress_callback: (e) => {
+          console.log(e);
+        },
+      });
+      const result = await generator("Hi", {
+        max_new_tokens: 20, // Limit the length of the generated text
+        temperature: 0.7, // Control the randomness of the generation
+        repetition_penalty: 2.0, // Help prevent model from outputting words in loop.
+      });
+      console.log(result);
     }
-  }
 
+    run();
+  }, []);
   return (
     <div className="container">
-      <h1>Text Generation</h1>
-      <input
-        type="text"
-        onChange={handleType}
-        onKeyDown={handleKeyDown}
-        disabled={isLoading}
-      />
-      <button
-        onClick={handleSubmit}
-        aria-busy={isLoading}
-        disabled={isLoading}
-        style={{ marginBottom: "1rem" }}
-      >
-        Submit
-      </button>
-      {modelStatus.modelName && (
-        <div>
-          <kbd
-            aria-busy={modelStatus.status === "progress"}
-            style={{ textTransform: "capitalize" }}
-          >
-            {modelStatus.modelName} ({modelStatus.status}:{modelStatus.progress}
-            %)
-          </kbd>
-        </div>
-      )}
-      {outputText && <article>{outputText}</article>}
+      <h1>Text Generation Test</h1>
     </div>
   );
 }
